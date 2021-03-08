9f969f7103aac5e0d37d205e88cce64563140e9d
Ashley Ruglys <ashley.ruglys@gmail.com>
2021-03-08 15:32:10 +0100

    Remove some branching from shadow stuff

--- shaders/shadows.glsl
+++ shaders/shadows.glsl
@@ -1,29 +1,23 @@
 uint cascade_index(float view_pos_z, vec3 splits) {
-    uint index = 0;
-
-    for (uint i = 0; i < 2; i++) {
-        if (view_pos_z < splits[i]) {
-            index = i + 1;
-        }
-    }
-
-    return index;
+	// Compare the z against the split distances. We only care about the first
+	// two. We want to find out how many splits the z is less than, as that's
+	// our cascade index.
+	bvec2 less_than_split = lessThan(vec2(view_pos_z), splits.xy);
+	// Convert this bvec2 into a vec of integers that are either 0 or 1.
+	uvec2 ints = uvec2(less_than_split);
+	// Sum the integers to get a count and return it.
+	uint count = ints.x + ints.y;
+	return count;
 }
 
 vec3 debug_colour_by_cascade(vec3 colour, uint cascade_index) {
-	switch(cascade_index) {
-		case 0:
-			colour *= vec3(1.0f, 0.25f, 0.25f);
-			break;
-		case 1:
-			colour *= vec3(0.25f, 1.0f, 0.25f);
-			break;
-		case 2:
-			colour *= vec3(0.25f, 0.25f, 1.0f);
-			break;
-	}
+	vec3 shading[3] = {
+		vec3(1.0, 0.25, 0.25),
+		vec3(0.25, 1.0, 0.25),
+		vec3(0.25, 0.25, 1.0)
+	};
 
-	return colour;
+	return colour * shading[cascade_index];
 }
 
 // See https://github.com/gfx-rs/wgpu-rs/blob/cadc2df8a106ad122c10c2e07733ade8f1e5653c/examples/shadow/shader.wgsl#L67
@@ -31,9 +25,11 @@ float calculate_shadow(float view_pos_z, mat4 matrices[3], vec3 splits, vec3 fra
 	uint cascade_index = cascade_index(view_pos_z, splits);
 	vec4 transformed_coords = matrices[cascade_index] * vec4(frag_pos, 1.0);
 
-    /*if (homogeneous_coords.w <= 0.0) {
+    /*
+	if (transformed_coords.w <= 0.0) {
         return 1.0;
-    }*/
+    }
+	*/
 
     // compensate for the Y-flip difference between the NDC and texture coordinates
     vec2 flip_correction = vec2(0.5, -0.5);
