e40de591387a0be40a13d0ce5eeb9d9e52f5ff64
Ashley Ruglys <ashley.ruglys@gmail.com>
2021-03-10 12:56:39 +0100

    Change compile_shaders.sh to use a loop instead of listing every shader

--- compile_shaders.sh
+++ compile_shaders.sh
@@ -1,39 +1,10 @@
 #!/bin/sh
 
-glslc shaders/scene.vert -o shaders/compiled/scene.vert.spv
-glslc shaders/scene.frag -o shaders/compiled/scene.frag.spv
+rm -r shaders/compiled/*.spv
 
-glslc shaders/ship.vert -o shaders/compiled/ship.vert.spv
-glslc shaders/ship.frag -o shaders/compiled/ship.frag.spv
-
-glslc shaders/sun_dir.vert -o shaders/compiled/sun_dir.vert.spv
-glslc shaders/particles.vert -o shaders/compiled/particles.vert.spv
-
-glslc shaders/flat_colour.frag -o shaders/compiled/flat_colour.frag.spv
-
-glslc shaders/fullscreen_tri.vert -o shaders/compiled/fullscreen_tri.vert.spv
-glslc shaders/tonemap.frag -o shaders/compiled/tonemap.frag.spv
-
-glslc shaders/scene_shadows.vert -o shaders/compiled/scene_shadows.vert.spv
-glslc shaders/ship_shadows.vert -o shaders/compiled/ship_shadows.vert.spv
-
-glslc shaders/ship_movement.comp -o shaders/compiled/ship_movement.comp.spv
-
-spirv-opt shaders/compiled/scene.vert.spv -O -o shaders/compiled/scene.vert.spv
-spirv-opt shaders/compiled/scene.frag.spv -O -o shaders/compiled/scene.frag.spv
-
-spirv-opt shaders/compiled/ship.vert.spv -O -o shaders/compiled/ship.vert.spv
-spirv-opt shaders/compiled/ship.frag.spv -O -o shaders/compiled/ship.frag.spv
-
-spirv-opt shaders/compiled/sun_dir.vert.spv -O -o shaders/compiled/sun_dir.vert.spv
-spirv-opt shaders/compiled/particles.vert.spv -O -o shaders/compiled/particles.vert.spv
-
-spirv-opt shaders/compiled/flat_colour.frag.spv -O -o shaders/compiled/flat_colour.frag.spv
-
-spirv-opt shaders/compiled/fullscreen_tri.vert.spv -O -o shaders/compiled/fullscreen_tri.vert.spv
-spirv-opt shaders/compiled/tonemap.frag.spv -O -o shaders/compiled/tonemap.frag.spv
-
-spirv-opt shaders/compiled/ship_movement.comp.spv -O -o shaders/compiled/ship_movement.comp.spv
-
-spirv-opt shaders/compiled/scene_shadows.vert.spv -O -o shaders/compiled/scene_shadows.vert.spv
-spirv-opt shaders/compiled/ship_shadows.vert.spv -O -o shaders/compiled/ship_shadows.vert.spv
+for file in shaders/*.{vert,frag,comp}
+do
+output=shaders/compiled/$(basename $file).spv
+glslc $file -o $output
+spirv-opt $output -O -o $output
+done
