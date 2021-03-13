aee566358ca20aacae11aa465d16d0cf185a4365
Ashley Ruglys <ashley.ruglys@gmail.com>
2021-03-13 22:04:26 +0100

    Change some rotor_to_matrix maths to use vec3 results

--- shaders/includes/rotor.glsl
+++ shaders/includes/rotor.glsl
@@ -6,12 +6,9 @@
 // https://github.com/termhn/ultraviolet/blob/9653d78b68aa19659b904d33d33239bbd2907504/src/rotor.rs#L606-L637
 mat3 rotor_to_matrix(Rotor r) {
     float s2 = r.s * r.s;
-    float bxy2 = r.bv.x * r.bv.x;
-    float bxz2 = r.bv.y * r.bv.y;
-    float byz2 = r.bv.z * r.bv.z;
-    float s_bxy = r.s * r.bv.x;
-    float s_bxz = r.s * r.bv.y;
-    float s_byz = r.s * r.bv.z;
+    vec3 bv2 = r.bv * r.bv;
+    vec3 s_bv = r.s * r.bv;
+
     float bxz_byz = r.bv.y * r.bv.z;
     float bxy_byz = r.bv.x * r.bv.z;
     float bxy_bxz = r.bv.x * r.bv.y;
@@ -20,18 +17,18 @@ mat3 rotor_to_matrix(Rotor r) {
 
     return mat3(
         vec3(
-            s2 - bxy2 - bxz2 + byz2,
-            -two * (bxz_byz + s_bxy),
-            two * (bxy_byz - s_bxz)),
+            s2 - bv2.x - bv2.y + bv2.z,
+            -two * (bxz_byz + s_bv.x),
+            two * (bxy_byz - s_bv.y)),
         vec3(
-            two * (s_bxy - bxz_byz),
-            s2 - bxy2 + bxz2 - byz2,
-            -two * (s_byz + bxy_bxz)
+            two * (s_bv.x - bxz_byz),
+            s2 - bv2.x + bv2.y - bv2.z,
+            -two * (s_bv.z + bxy_bxz)
         ),
         vec3(
-            two * (s_bxz + bxy_byz),
-            two * (s_byz - bxy_bxz),
-            s2 + bxy2 - bxz2 - byz2
+            two * (s_bv.y + bxy_byz),
+            two * (s_bv.z - bxy_bxz),
+            s2 + bv2.x - bv2.y - bv2.z
         )
     );
 }
